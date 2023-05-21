296c2d458f4e3e09000564de5936782827c637b2
programmerasif <mdasifkhan3757@gmail.com>
2023-05-21 10:48:09 +0600

    inviroment variable done

--- Assignment11/src/Fairbase/Fairbase.config.js
+++ Assignment11/src/Fairbase/Fairbase.config.js
@@ -5,14 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDStJcpiSx-ddjONwUkuYD8HA4-PzeZAhs",
-  authDomain: "assignment-11-5a444.firebaseapp.com",
-  projectId: "assignment-11-5a444",
-  storageBucket: "assignment-11-5a444.appspot.com",
-  messagingSenderId: "340594982385",
-  appId: "1:340594982385:web:d3c985f667b33fab105850"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
-
+console.log(import.meta.env.VITE_apiKey);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
